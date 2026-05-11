4116ae08e0e6dcf8e32ecf53136ed68d041e42b1
SuhaniGupta2212 <2023nitsgr170@nitsri.ac.in>
2026-05-12 02:01:41 +0530

    Updated branding and UI

--- app/store/layout.jsx
+++ app/store/layout.jsx
@@ -1,8 +1,8 @@
 import StoreLayout from "@/components/store/StoreLayout";
 
 export const metadata = {
-    title: "GoCart. - Store Dashboard",
-    description: "GoCart. - Store Dashboard",
+    title: "KartVerse. - Store Dashboard",
+    description: "KartVerse. - Store Dashboard",
 };
 
 export default function RootAdminLayout({ children }) {
